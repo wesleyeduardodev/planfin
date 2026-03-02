45506e32550375bbe0b2063902abd3e38463c85a
Wesley Eduardo <111645247+wesleyeduardodev@users.noreply.github.com>
2026-03-02 20:59:14 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth"
 import { authConfig } from "@/lib/auth.config"
 
-export const { auth: middleware } = NextAuth(authConfig)
+const { auth } = NextAuth(authConfig)
+
+export const middleware = auth
 
 export const config = {
   matcher: [
